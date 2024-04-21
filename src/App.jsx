2884f33f94fcafa03ffd00031cf0b05781830435
Sarfaroj Ansari <sarfarosha66@gmail.com>
2024-04-22 00:41:52 +0530

    add title & clear unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Cards from './components/Cards';
